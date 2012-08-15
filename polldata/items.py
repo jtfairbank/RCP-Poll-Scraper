0d24a5cee6db913eaaaac6d280e41c979c903a8d
jtfairbank <jtfairbank@gmail.com>
2012-08-15 11:00:19 -0500

    Setup Scrapy and Define the Presidential Poll Item

---
+++ polldata/items.py
@@ -0,0 +1,16 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/topics/items.html
+
+from scrapy.item import Item, Field
+
+class PresPollItem(Item):
+    # define the fields for your item here like:
+    service = Field()
+    start   = Field()
+    end     = Field()
+    sample  = Field()
+    dem     = Field()
+    rep     = Field()
+    ind     = Field()
