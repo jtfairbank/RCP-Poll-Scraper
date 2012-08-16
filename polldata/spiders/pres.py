727db3025bc1a83c4f255f19cea006e686850689
jtfairbank <jtfairbank@gmail.com>
2012-08-15 21:29:31 -0500

    [NOT WORKING] Implement Presidential Poll Spider
    This presidential poll spider implementation extends the CrawlSpider
    class.  Scrapy recommends this for most crawling spiders, but it doesn't
    seem to work in this case.  The main issue is that none of the links
    matched by the specified regex are followed, nor are any of the callback
    functions called.  It could be due to a semantic error in the pres2012
    spider or an issue with Scrapy itself.

---
+++ polldata/spiders/pres.py
@@ -0,0 +1,45 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from scrapy.item import Item
+
+class PresSpider(CrawlSpider):
+    name = "pres2012"
+    allowed_domains = ["realclearpolitics.com"]
+    start_urls = [
+        "http://www.realclearpolitics.com/epolls/latest_polls/president/"
+    ]
+
+    rules = (
+        Rule(
+            SgmlLinkExtractor(
+                allow=(r"epolls/2012/president/[a-z]{2}/[a-z]+_romney_vs_obama_-[0-9]{4}\.html"),
+                # Regex explanation:
+                #     [a-z]{2} - matches a two character state abbreviation
+                #     [a-z]*   - matches a state name
+                #     [0-9]{4} - matches a 4 number unique webpage identifier
+
+                allow_domains=('realclearpolitics.com',),
+            ),
+            callback='parseStatePolls',
+            process_links='processLinks',
+            process_request='processRequest',
+        ),
+    )
+
+
+    def parseStatePolls(self, response):
+        self.log('Hi, this is an item page! %s' % response.url, level=log.DEBUG)
+        pass
+
+    # filters out repeat state poll links
+    # ie only get new polls from Ohio once
+    def processLinks(links):
+        print links
+        return links
+
+    # filters out states that don't have any polling data
+    # probably shouldn't worry about this as all latest poll states will have a poll
+    # TODO: remove this function and process_request filed from Rules
+    def processRequest(request):
+        return request
