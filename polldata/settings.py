f5301ea5147ef487817a4039d8ad72b9d49e25a4
jtfairbank <jtfairbank@gmail.com>
2012-10-13 17:23:37 -0500

    Remove outdated setting.

--- polldata/settings.py
+++ polldata/settings.py
@@ -9,16 +9,14 @@
 from datetime import datetime
 
 BOT_NAME = 'polldata'
-BOT_VERSION = '1.0'
 
 SPIDER_MODULES = ['polldata.spiders']
 NEWSPIDER_MODULE = 'polldata.spiders'
-USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
 
 ITEM_PIPELINES = [
     'polldata.pipelines.CsvExportPipeline',
 ]
 
-LOG_FILE = 'logs/%s | %s | %s.txt' % (datetime.today(), BOT_NAME, BOT_VERSION)
+LOG_FILE = 'logs/%s | %s.txt' % (datetime.today(), BOT_NAME)
 
 ROBOTSTXT_OBEY = True
