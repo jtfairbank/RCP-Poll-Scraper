5bbc099eb374a36a858f0bdb5979993c2cf2693c
Bhargee <Bhargee.Manja@gmail.com>
2012-10-12 22:44:36 -0500

    Output works kind of, gonna go for a run and test for bugs

--- polldata/pipelines.py
+++ polldata/pipelines.py
@@ -5,7 +5,7 @@
 
 from scrapy import signals
 from scrapy.contrib.exporter import CsvItemExporter
-
+import json
 '''
 Exports Poll Items into a CSV file in an order defined by the existing process.
 
@@ -31,6 +31,11 @@ class CsvExportPipeline(object):
 
     def spider_opened(self, spider):
         file = open('data/pres_latest.csv', 'w+')
+        self.dict_file = open('data/dict.json', 'wr+')
+        try:
+            self.objs = json.load(self.dict_file)
+        except (ValueError):
+            self.objs = {}
         self.files[spider] = file
         self.exporter = CsvItemExporter(file, fields_to_export=spider.fields_to_export)
         self.exporter.start_exporting()
@@ -39,7 +44,15 @@ class CsvExportPipeline(object):
         self.exporter.finish_exporting()
         file = self.files.pop(spider)
         file.close()
+        self.dict_file.write(str(self.objs))
+        self.dict_file.close()
 
     def process_item(self, item, spider):
-        self.exporter.export_item(item)
-        return item
+        identifier = item['dem']+item['end']+item['rep']+str(item['ind'])+item['sample']+item['service']
+        obj_hash = hash(identifier)
+        if obj_hash not in self.objs:
+            self.objs[obj_hash] = item
+            self.exporter.export_item(item)
+            return item
+        else:
+            return None
