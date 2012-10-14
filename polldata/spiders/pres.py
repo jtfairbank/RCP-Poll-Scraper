8c12749e73b837def5cefc7128a305ab491e4ada
jtfairbank <jtfairbank@gmail.com>
2012-10-14 15:59:29 -0500

    Create Documentation for pres2012 Spider

--- polldata/spiders/pres.py
+++ polldata/spiders/pres.py
@@ -7,6 +7,20 @@ from scrapy import log
 from polldata.items import PresPollItem
 
 class PresSpider(CrawlSpider):
+    """Create State-Level Presidential Polls
+
+    PresSpider crawls pages on RealClearPolitics.com with Obama vs. Romney
+    state-level polls, and produces pollitems based on the polls listed on those
+    pages.  The source for these pages (how each are found) is the select box
+    found in the top of http://www.realclearpolitics.com/epolls/latest_polls/president/.
+
+    Assumptions:
+        Certain html tags and attributes exist in order to identify where on the
+        page the polling data is located.
+
+        Assumes a certain structure to the polling data container (its a table
+        with certain column headers) in order to extract each poll's data.
+    """
     name = "pres2012"
     allowed_domains = ["realclearpolitics.com"]
     start_urls = [
@@ -25,6 +39,9 @@ class PresSpider(CrawlSpider):
                 #     [a-z]*   - matches a state name
                 #     [0-9]{4} - matches a 4 number unique webpage identifier
 
+                # TODO: does this regex always hold for '_romney_vs_obama-' or
+                #       may obama come first sometimes?
+
                 allow_domains=('realclearpolitics.com',),
             ),
             callback='parseStatePolls',
@@ -35,9 +52,22 @@ class PresSpider(CrawlSpider):
     )
 
     # TODO: use a custom link extractor to crawl //*[@id="search_by_race"]/optgroup[1]/*
+    #       instead of links on the page
 
 
     def parseStatePolls(self, response):
+        """Find pollitems for a state.
+
+        For every poll in this state: find the poll's data and process it into
+        a pollitem.
+
+        Args:
+            response
+                An html response that contains the presidential polling data for a state.
+
+        Returns:
+            A list of pollitems that each represent a unique state-level poll.
+        """
         items = []
         hxs = HtmlXPathSelector(response)
 
@@ -75,6 +105,28 @@ class PresSpider(CrawlSpider):
         return items
 
     def _getDataPositions(self, headers):
+        """Determine the columns that each pollitem field's values are in.
+
+        Args:
+            headers
+                An array of the text values for the Polling Data table headers.
+                Ex: ["Poll", "Date", "Sample", "MoE", "Romney (R)", "Obama (D)", "Spread"]
+
+        Returns
+            A lookup dict that maps from pollitem fields to the related data's
+            column position (an integer).
+
+            Ex: {   "service": 0,
+                    "date": 1,
+                    "sample": 2,
+                    "error": 3,
+                    "dem": 5,
+                    "rep": 4,
+                }
+        """
+        # TODO: create a per-spider mapping from the table header's text values
+        #       to the pollitem's fields?
+        #       Also, pull this functionality out to a common utility class?
         lookup = {}
 
         i = 0
@@ -104,8 +156,9 @@ class PresSpider(CrawlSpider):
         Args:
             dateText
                 A string that describes the dates the poll was conducted
-                    during by providing a range from the poll's start date to
-                    its end date.  The year is ommitted.
+                during by providing a range from the poll's start date to
+                its end date.  The year is ommitted.
+
                 Ex: "10/1 - 10/3"
                     "5/3"
 
@@ -113,19 +166,21 @@ class PresSpider(CrawlSpider):
             An array with two entries:
                 1: the poll's start date
                 2: the poll's end date
-            If there is only one date (eg. the poll was conducted during one
-                day, or information is unknown) then that date is used as the
-                end date and the start date is left as an empty string.
-            The current year is appended to the end of the poll dates.  This is
-                problematic since polls conducted in previous years are also
-                listed.
-            Ex. ["10/1/2012", "10/3/2012"]
-                ["", "5/3/2012"]
 
+            If there is only one date (eg. the poll was conducted during one
+            day, or information is unknown) then that date is used as the
+            end date and the start date is left as an empty string.
+
+            The current year is appended to the end of the poll dates.  This is
+            problematic since polls conducted in previous years are also listed.
+
+            Ex: ["10/1/2012", "10/3/2012"]
+                ["", "5/3/2012"]
         """
         # TODO: attempt to intelligently determine the year, instead of assuming
         #       that all polls occur in the current year?
-        #       Are there any polls from previous years?
+        #       Are there any polls from previous years? YES
+        # TODO: return multiple items instead of an array of items
         daterange = dateText.split(' - ')
 
         # BugFix w/ If Statement and Array Resize
@@ -143,13 +198,45 @@ class PresSpider(CrawlSpider):
         return daterange
 
     def _parseSampleInfo(self, sampleInfoText):
+        """Find the sample size and sample type of a poll.
+
+        Break a sample info string into its sample size and sample type
+        components.
+
+        Args:
+            sampleInfoText
+                A string that describes the population sampled by the poll.
+
+                Ex: "595 LV"
+                    "1337"
+                    "RV"
+
+        Returns:
+            An array with two entries:
+                1: the poll's sample size
+                2: the poll's sample type (usually 'LV': likely voters, or 'RV':
+                    registered voters)
+            If there is only one component in the sample information (eg. the
+                poll's sample size is unknown) then that component is used as
+                the sample type and the sample size is left as an empty string.
+                This is problematic because there are cases where sample size is
+                the only component, thus the assumption is wrong.
+            Ex: ["595", "LV"]
+                ["", "1337"]
+                ["", "RV"]
+        """
+        # TODO: attempt to intelligently guess which component is present if
+        #       only one is.  This can be done by trying to read the component
+        #       as an integer (which will work for sample size but not sample
+        #       type).
+        # TODO: return multiple items instead of an array of items
         sampleInfo = sampleInfoText.split(' ')
 
         # BugFix w/ If Statement
         #  - Prevents errors when either the sample size or the sample type
         #       (RV: registered voters, or LV: likely voters)
         #    is missing, thus there is only one component in sampleInfo which
-        #    is assumed to be the sample size.
+        #    is assumed to be the sample type.
         if sampleInfo[0] is None:
             sampleInfo[0] = ''
 
@@ -162,11 +249,21 @@ class PresSpider(CrawlSpider):
     def processLinks(self, links):
         """
         Remove links to states with no polls.
+
+        Args:
+            links
+                A list of links to follow, each of which represents a different
+                state, which may or may not contain polling data.
+
+        Returns
+            A list of links to follow, each of which represents a different
+            state that has polling data.
         """
         noPollLink = "http://www.realclearpolitics.com/epolls/2012/president/2012_elections_electoral_college_map.html"
 
+        # TODO: remove without for loop?  ie links.remove(...) would remove all instances?
         for link in links:
-            if link == noPollLink
+            if link == noPollLink:
                 links.remove(link)
 
         return links
